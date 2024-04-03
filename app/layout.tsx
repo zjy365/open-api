8dc0a28ac5c5c17432758c7db5221ee6da3562cc
jingyang <3161362058@qq.com>
2024-04-03 14:16:41 +0800

    session

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
+import { createSealosApp } from '@zjy365/sealos-desktop-sdk/app';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,6 +16,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const response = createSealosApp();
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
